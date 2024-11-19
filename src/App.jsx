fc6c8d2dc3bb117619f253c868dcf76f4c150ffc
Aqil Jaafree <wanaqilre@gmail.com>
2024-11-20 01:07:04 +0800

    soon hack

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+import Navbar from './Navbar';
+import GameComponent from './GameComponent';
+
+import '@solana/wallet-adapter-react-ui/styles.css';
+import './App.css';
+
+function App() {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = "https://rpc.devnet.soo.network/rpc";
+
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+    []  // Removed network dependency since we're using custom endpoint
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <div className="App">
+            <Navbar />
+            <main>
+              <GameComponent />
+            </main>
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
+
+export default App;
